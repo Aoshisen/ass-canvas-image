18b1c03dfed733a1f8fa0167c3f296fc22c52b71
aoshisen <aoshisen.23@gmail.com>
2024-10-31 23:02:53 +0800

    feat: 滚动重组以及消散动画

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-// import Hamburg from "./components/Hamburg/index.vue"
 import Woman from "./components/Test/index.vue"
 </script>
 <template>
-  <!-- <Hamburg /> -->
   <Woman />
 </template>
--- src/components/Test/index.vue
+++ src/components/Test/index.vue
@@ -1,39 +1,42 @@
 <script setup lang="ts">
-import { Color } from "pixi.js";
 import img from "./woman.jpg";
-import { ref, reactive } from "vue"
+import { ref, reactive, onMounted } from "vue"
+import { randomUniform, color as d3Color, RGBColor } from "d3";
+import { useEventListener, useThrottleFn } from "@vueuse/core";
 
 interface Point {
 	x: number;
 	y: number;
-	color: Color;
-	radius: number
+	color: RGBColor;
+	radius: number;
+	offsetX: number;
+	offsetY: number;
 }
 
 const GAP = 4;
-const imgElement = ref()
 const canvasElement = ref()
-
-const canvasWidth = ref(800);
-const canvasHeight = ref(500)
 
 let points = reactive<Point[]>([])
 
-function handleImageLoaded() {
-	const data = getImageData(imgElement.value)
+function handleImageLoaded(this: HTMLImageElement) {
+	const data = getImageData(this)
 	points = getRenderPoint(data, GAP);
-	drawPoints.bind(canvasElement.value)(points)
+	drawPoints.bind(canvasElement.value)(points as Point[])
+}
+
+function animation(step: number) {
+	const currentPoints = points.map((point) => getNextPoint(point as Point, step))
+	drawPoints.bind(canvasElement.value)(currentPoints as Point[])
 }
 
 function getImageData(image: HTMLImageElement) {
 	const canvas = document.createElement("canvas");
-	canvas.width = image.width;
-	canvas.height = image.height;
+	const W = canvas.width = image.width;
+	const H = canvas.height = image.height;
 	const ctx = canvas.getContext("2d")
 	ctx!.drawImage(image, 0, 0);
-	// 获取图像数据
-	const imageData = ctx!.getImageData(0, 0, canvas.width, canvas.height);
-	return imageData
+	const imageData = ctx!.getImageData(0, 0, W, H);
+	return imageData;
 }
 
 function getRenderPoint(data: ImageData, gap: number): Point[] {
@@ -43,19 +46,15 @@ function getRenderPoint(data: ImageData, gap: number): Point[] {
 	for (let y = 0; y < height; y += gap) {
 		for (let x = 0; x < width; x += gap) {
 			const index = (y * width + x) * 4; // 每个像素占4个值 (RGBA)
-
 			const r = data.data[index];
 			const g = data.data[index + 1];
 			const b = data.data[index + 2];
 			const a = data.data[index + 3];
-
-			// 将 RGB 转换为 CSS 格式
-			// const color = `rgba(${r}, ${g}, ${b}, ${a})`;
-			const color = new Color({ r, g, b, a })
-			const radius = getGrayScaledRadius(color)
-
-			// 将点添加到数组
-			_points.push({ x, y, color, radius });
+			const color = d3Color(`rgba(${r},${g},${b},${a})`) as RGBColor;
+			const radius = getGrayScaledRadius(color);
+			const offsetX = randomUniform(0, 6)();
+			const offsetY = randomUniform(0, 6)();
+			_points.push({ x, y, color, radius, offsetX, offsetY });
 		}
 	}
 
@@ -69,34 +68,49 @@ function drawPoint(this: CanvasRenderingContext2D, point: Point) {
 	this.fill();
 }
 
-function getGrayScaledRadius(color: Color) {
-	const grayScale = gray.call(color)
+function getGrayScaledRadius(color: RGBColor) {
+	const grayScale = gray.call(color) / 255;
 	const radius = GAP / 2 * (1 - grayScale);
-	return radius
+	return radius;
 }
 
-function gray(this: Color) {
-	return 0.299 * this.red + 0.587 * this.green + 0.114 * this.blue
+function gray(this: RGBColor) {
+	return 0.299 * this.r + 0.587 * this.g + 0.114 * this.b
 }
 
 function drawPoints(this: HTMLCanvasElement, points: Point[]) {
 	const ctx = this.getContext("2d");
 	if (!ctx) return;
-
-	// 清空 canvas
 	ctx.clearRect(0, 0, this.width, this.height);
+	points.forEach(point => drawPoint.bind(ctx)(point))
+}
 
-	// 绘制每个点
-	for (let index = 0; index < points.length; index++) {
-		const point = points[index];
-		drawPoint.bind(ctx)(point);
-	}
+function handleMounted() {
+	const image = new Image()
+	image.src = img;
+	image.onload = handleImageLoaded.bind(image);
 }
 
 
+function getNextPoint(point: Point, step: number) {
+	return {
+		...point,
+		x: point.x + point.offsetX * step,
+		y: point.y + point.offsetY * step
+	};
+}
+
+function scroll() {
+	animation(window.scrollY / 20)
+}
+
+onMounted(handleMounted)
+const throttledScroll = useThrottleFn(scroll, 20)
+useEventListener(window, "scroll", throttledScroll)
 </script>
 
 <template>
-	<canvas class="canvas" ref="canvasElement" :width="canvasWidth" :height="canvasHeight"></canvas>
-	<img :src="img" @load="handleImageLoaded" ref="imgElement" hidden />
+	<div style="height:200px"></div>
+	<canvas ref="canvasElement" width="800" height="600"></canvas>
+	<div style="height:1200px"></div>
 </template>
